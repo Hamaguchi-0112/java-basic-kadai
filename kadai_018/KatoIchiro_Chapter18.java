121177e44342a3a7e9190fb513a7241e130b0421
Hamaguchi-0112 <smfc7se2@gmail.com>
2025-07-13 14:13:45 +0900

    kadai_018

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -1,9 +1,16 @@
 package kadai_018;
 
-public class KatoIchiro_Chapter18 extends Kato_Chapter18{
-	public void eachIntroduce() {
-		setGivenName("一郎");
-		System.out.println("好きな食べ物はりんごです");
-	}	
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+    
+    public KatoIchiro_Chapter18() {
+        setGivenName();
+    }
+    
+    public void setGivenName() {
+        this.givenName = "一郎";
+    }
 
+    public void eachIntroduce() {
+        System.out.println("好きな食べ物はりんごです");
+    }
 }
